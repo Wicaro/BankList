0c0635e32b477299f7717b095234dced5645f8de
Wicaro <kodadeveloper@hotmail.com>
2023-04-07 16:31:33 -0300

    Página de listagem dos bancos

--- src/Core/Router/Router.tsx
+++ src/Core/Router/Router.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, createBrowserRouter, Route } from "react-router-dom";
-import Index from "../../Interface/Pages/Index/Index";
+import Index from "@/Interface/Pages/Index/Index";
+import Banks from "@/Interface/Pages/Banks/Banks";
 
 // createBrowserRouter é uma função que retorna um objeto do tipo Router
 // que contém as rotas da aplicação.
@@ -9,5 +10,9 @@ export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Index />,
+  },
+  {
+    path: "/banks",
+    element: <Banks />,
   }
 ]);
---
+++ src/Interface/Pages/Banks/Banks.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Banks = () => {
+  return (
+    <Container>
+      <p className="mt-5">Listagem dos bancos</p>
+    </Container>
+  );
+};
+
+export default Banks;
