eeba859d6fc80d740080805b3793c7955cd4dc90
Wicaro <kodadeveloper@hotmail.com>
2023-04-07 16:36:08 -0300

    BankHook

---
+++ src/Core/Hooks/BankHook.ts
@@ -0,0 +1,8 @@
+import React from "react";
+import { BankContext } from "../Providers/BankProvider";
+import { BankContextType } from "../@types/BankType";
+
+export default function useBank(){
+    const context =  React.useContext(BankContext) as BankContextType
+    return context
+}
